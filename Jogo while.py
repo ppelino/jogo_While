8668874ee8333869e651192a5185e0bf632aab8b
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2022-05-18 22:06:51 -0300

    Add files via upload

---
+++ Jogo while.py
@@ -0,0 +1,14 @@
+numero_secreto = 42
+total_de_tentativas = 3
+while (total_de_tentativas > 0):
+    chute = int(input('Digite	o	seu	número:	'))
+    print('Você	digitou:	', chute)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+    if (acertou):
+        print('Você	acertou')
+    elif (maior):
+        print('Você	errou!	O	seu	chute	foi	maior	que	o	número	secreto')
+    elif (menor):
+        print('Você	errou!	O	seu	chute	foi	menor	que	o	número	secreto')
